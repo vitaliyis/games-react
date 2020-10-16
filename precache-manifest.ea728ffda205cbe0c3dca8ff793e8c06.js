d795fad7aee655c224fe451b5bf4163f94c16ff3
vitaliyis <vetal.friend@gmail.com>
2020-10-16 11:11:18 +0300

    Updates

---
+++ precache-manifest.ea728ffda205cbe0c3dca8ff793e8c06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cbe95f831dc1f8ae030360c4484e1a",
+    "url": "/games-react/index.html"
+  },
+  {
+    "revision": "2b855ffe656f338aae71",
+    "url": "/games-react/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "7e2feaa897d9ecbff910",
+    "url": "/games-react/static/css/main.8db70f43.chunk.css"
+  },
+  {
+    "revision": "2b855ffe656f338aae71",
+    "url": "/games-react/static/js/2.ebfbfd2b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/games-react/static/js/2.ebfbfd2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2feaa897d9ecbff910",
+    "url": "/games-react/static/js/main.86260aa6.chunk.js"
+  },
+  {
+    "revision": "395c6aafeab444c68863",
+    "url": "/games-react/static/js/runtime-main.5fa0ec43.js"
+  },
+  {
+    "revision": "fcfaaffa1c39b3ce751bba445533607f",
+    "url": "/games-react/static/media/vetal.fcfaaffa.jpg"
+  }
+]);
